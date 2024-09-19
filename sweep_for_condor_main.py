67ff3660baa0e7a3a1768e9513aa5189a5cac2a6
Hsu Chen-Hua <ken91021615@gmail.com>
2024-09-19 07:40:52 +0200

    full dataset without 130

--- sweep_for_condor_main.py
+++ sweep_for_condor_main.py
@@ -22,8 +22,8 @@ def prepare_shell(shell_file, command, condor, FarmDir, afs_dir):
 if __name__ == '__main__':
   usage = 'usage: %prog[options]'
   parser = argparse.ArgumentParser(description=usage)
-  parser.add_argument('--config_file', type=str, default = '/eos/user/c/chenhua/copy_tdsm_encoder_sweep16/configs/each_epoch_generate.yml',help = 'configuration file for wandb')
-  parser.add_argument('--python_cfg', type=str, default = 'trans_tdsm_130_generate.py', help = 'python file to run the code')
+  parser.add_argument('--config_file', type=str, default = '/eos/user/c/chenhua/copy_tdsm_encoder_sweep16/configs/full_dataset_without_130.yml',help = 'configuration file for wandb')
+  parser.add_argument('--python_cfg', type=str, default = 'trans_tdsm_main.py', help = 'python file to run the code')
   parser.add_argument('--n_run', type=int, default = 2, help = 'number of runs')
   parser.add_argument('--dryRun', action='store_true', help = 'not submit to condor')
   parser.add_argument('--afs_dir', type=str, default = '/afs/cern.ch/user/c/chenhua', help='workspace in afs space')
@@ -38,10 +38,10 @@ if __name__ == '__main__':
   CWD = os.getcwd()
   sweep_yml['program'] = os.path.join(CWD, args.python_cfg)
   sweep_yml['parameters']['work_dir'] = {'value': CWD}
-  sweep_yml['parameters']['switches'] = {'value': '1100'}
+  sweep_yml['parameters']['switches'] = {'value': '1110'}
   sweep_yml['parameters']['condor']   = {'value': 1}
-  sweep_yml['parameters']['inputs']= {'value': os.path.join(CWD, "fulldataset")}
-  sweep_yml['parameters']['preprocessor'] = {'value': os.path.join(CWD, "fulldataset/dataset_2_padded_transform_incident_later_nentry2065To2193_preprocessor.pkl")}
+  sweep_yml['parameters']['inputs']= {'value': os.path.join(CWD, "fulldataset_without_130")}
+  sweep_yml['parameters']['preprocessor'] = {'value': os.path.join(CWD, "fulldataset_without_130/dataset_2_padded_transform_incident_later_nentry2065To2193_preprocessor.pkl")}
 
 
   # Create necessary work space in afs space (condor can not be submitted from eos space)
