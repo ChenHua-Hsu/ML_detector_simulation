0845dce735c1ef3d1ac2c4c3f54f8bbc940bae32
Hsu Chen-Hua <ken91021615@gmail.com>
2024-09-28 15:46:45 +0200

    #ignore

--- sweep_for_condor_main.py
+++ sweep_for_condor_main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
   CWD = os.getcwd()
   sweep_yml['program'] = os.path.join(CWD, args.python_cfg)
   sweep_yml['parameters']['work_dir'] = {'value': CWD}
-  sweep_yml['parameters']['switches'] = {'value': '1100'}
+  sweep_yml['parameters']['switches'] = {'value': '1110'}
   sweep_yml['parameters']['condor']   = {'value': 1}
   sweep_yml['parameters']['inputs']= {'value': os.path.join(CWD, "fulldataset")}
   sweep_yml['parameters']['preprocessor'] = {'value': os.path.join(CWD, "fulldataset/dataset_2_padded_transform_incident_later_nentry2065To2193_preprocessor.pkl")}
