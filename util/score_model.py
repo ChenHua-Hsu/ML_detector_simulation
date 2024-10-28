65160def03525f0cedb577a71a5c8848caae094b
Hsu Chen-Hua <ken91021615@gmail.com>
2024-10-28 12:55:30 +0100

    .

--- util/score_model.py
+++ util/score_model.py
@@ -157,8 +157,8 @@ class Gen(nn.Module):
         # 'class' token (mean field)
         x_cls = self.cls_token.expand(x.size(0), 1, -1)
 
-        print(self.dense_t(embed_t_).shape)
-        print(self.dense_e(embed_e_).shape)
+        #print(self.dense_t(embed_t_).shape)
+        #print(self.dense_e(embed_e_).shape)
 
         
         # Feed input embeddings into encoder block
