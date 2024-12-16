166cb05ba7aa387f961ab10f8715d9da7aa9d488
Hsu Chen-Hua <ken91021615@gmail.com>
2024-12-16 15:28:47 +0100

    fix latex

--- env.sh
+++ env.sh
@@ -5,7 +5,9 @@ centOSversion=$(awk -F= '/VERSION_ID/ {split($2, version, "."); print version[1]
 echo $centOSversion$
 if [ "$centOSversion" -eq 9 ]; then
 	#source /cvmfs/sft.cern.ch/lcg/views/LCG_105a_cuda/x86_64-el9-gcc11-opt/setup.sh 
-	source /cvmfs/sft.cern.ch/lcg/views/LCG_104a_cuda/x86_64-el9-gcc11-opt/setup.sh 
+	#source /cvmfs/sft.cern.ch/lcg/views/LCG_104a_cuda/x86_64-el9-gcc11-opt/setup.sh
+	#source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos9-gcc11-opt/setup.sh 
+	source /cvmfs/sft.cern.ch/lcg/views/LCG_105a/x86_64-el9-gcc11-opt/setup.sh	
 else
 	source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_cuda/x86_64-centos7-gcc11-opt/setup.sh
 fi
