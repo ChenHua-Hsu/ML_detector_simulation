8c221fbaf95f31b6e781f832ce4f6ea80a409498
Hsu Chen-Hua <ken91021615@gmail.com>
2024-12-17 14:16:58 +0100

    .

--- env.sh
+++ env.sh
@@ -4,10 +4,10 @@
 centOSversion=$(awk -F= '/VERSION_ID/ {split($2, version, "."); print version[1]}' /etc/os-release | tr -d '"')
 echo $centOSversion$
 if [ "$centOSversion" -eq 9 ]; then
-	#source /cvmfs/sft.cern.ch/lcg/views/LCG_105a_cuda/x86_64-el9-gcc11-opt/setup.sh 
+	source /cvmfs/sft.cern.ch/lcg/views/LCG_105a_cuda/x86_64-el9-gcc11-opt/setup.sh 
 	#source /cvmfs/sft.cern.ch/lcg/views/LCG_104a_cuda/x86_64-el9-gcc11-opt/setup.sh
 	#source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos9-gcc11-opt/setup.sh 
-	source /cvmfs/sft.cern.ch/lcg/views/LCG_105a/x86_64-el9-gcc11-opt/setup.sh	
+	#source /cvmfs/sft.cern.ch/lcg/views/LCG_105a/x86_64-el9-gcc11-opt/setup.sh	
 else
 	source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_cuda/x86_64-centos7-gcc11-opt/setup.sh
 fi
